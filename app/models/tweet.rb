4e4aa3a58921664342c104ce3dbc7d8290f4cb3b
Ivan M. Malo <imalo@student.unimelb.edu.au>
2015-12-09 13:26:40 +1100

    Add map to coordinates. Add filter by country in couchdb views. Add image to home page

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,9 +8,29 @@ class Tweet < CouchRest::Model::Base
 
   design do
 
-    reduce_sum = "function(keys, values, rereduce) {
+    REDUCE_SUM = "function(keys, values, rereduce) {
           return sum(values);
         }"
+
+    INSIDE_BOX = 'function inside_box (point, bbox) {
+    
+    // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+    
+    var x = point[0], y = point[1];
+    
+    var inside = false;
+    
+    for (var i = 0, j = bbox.length - 1; i < bbox.length; j = i++) {
+        var xi = bbox[i][0], yi = bbox[i][1];
+        var xj = bbox[j][0], yj = bbox[j][1];
+        
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    
+    return inside;
+};'
     view :by_profile,:map =>"function(doc) {
       var name = doc.tweet_data.user.screen_name;
         if (name){
@@ -26,18 +46,21 @@ class Tweet < CouchRest::Model::Base
       }
     }"
     # Most Mentioned User
-    view :by_most_mentioned,:map => "function(doc) {
+    view :by_most_mentioned,:map => 'function(doc) {
+    if(doc.tweet_data.place.country_code == "EC"){
     if(doc.tweet_data.entities){
         if(doc.tweet_data.entities.user_mentions){
             doc.tweet_data.entities.user_mentions.forEach(function(user){
-                emit('@'+user.screen_name, 1)
+                emit("@"+user.screen_name, 1)
             });
         }
     }
-    }",:reduce => reduce_sum
+  }
+    }',:reduce => REDUCE_SUM
     
     # All mentioned hash tag topics
     view :hashtag, :map => 'function(doc) {
+    if(doc.tweet_data.place.country_code == "EC"){
     if(doc.tweet_data.entities){
         if(doc.tweet_data.entities.hashtags){
             doc.tweet_data.entities.hashtags.forEach(function(hashtag){
@@ -45,21 +68,27 @@ class Tweet < CouchRest::Model::Base
             });
         }
     }
-}', :reduce => reduce_sum
+  }
+}', :reduce => REDUCE_SUM
 
     # Source use for tweeting
     view :by_source, :map => 'function(doc) {
         if (doc.tweet_data.source){
         emit(doc.tweet_data.source, 1);
       }
-    }', :reduce => reduce_sum
+    }', :reduce => REDUCE_SUM
 
     # Identify if the tweet have coordinate
     view :by_coordinates, :map => 'function(doc) {
         if (doc.tweet_data.coordinates){
-        emit(doc.tweet_data._id, doc.tweet_data.coordinates);
-      }
-    }'
+          bounding_box = [[-80.0990009,-2.2873981],[-80.0990009,-2.0173767],[-79.8608184,-2.0173767],[-80.0990009,-2.2873981],[-80.0990009,-2.2873981]]
+          point = doc.tweet_data.coordinates.coordinates;
+          if (inside_box(point,bounding_box)) {
+    emit(doc.tweet_data._id, doc.tweet_data.coordinates);
+  }
+          
+      }'+ INSIDE_BOX +
+    '}'
 
   end
 end
