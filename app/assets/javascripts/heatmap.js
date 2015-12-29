24fb8fe4141b54bdef033907691b7460b7aaab4a
Ivan M. Malo <imalo@student.unimelb.edu.au>
2015-12-30 00:59:00 +1100

    Add gradient and radius to heatmap

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -12,7 +12,9 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('heatmap'), {
     center: {lat: -2.1649033, lng: -79.9260202},
     zoom: 12,
-    dissipating: false
+    dissipating: false,
+    radius: 20,
+    gradient: gradient
   });
 
   heatmap = new google.maps.visualization.HeatmapLayer({
@@ -20,6 +22,24 @@ function initMap() {
     map: map
   });
 }
+
+var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ];
+
 
   
   
