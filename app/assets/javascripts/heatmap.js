e43c4fa0d3f454794e051f6f87888e3f83e1dd4b
Ivan M. Malo <imalo@student.unimelb.edu.au>
2015-12-30 01:24:47 +1100

    Add radius, delete gradient from heatmap

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -12,9 +12,8 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('heatmap'), {
     center: {lat: -2.1649033, lng: -79.9260202},
     zoom: 12,
-    dissipating: false,
-    radius: 20,
-    gradient: gradient
+    dissipating: true,
+    radius: 200
   });
 
   heatmap = new google.maps.visualization.HeatmapLayer({
@@ -23,22 +22,7 @@ function initMap() {
   });
 }
 
-var gradient = [
-    'rgba(0, 255, 255, 0)',
-    'rgba(0, 255, 255, 1)',
-    'rgba(0, 191, 255, 1)',
-    'rgba(0, 127, 255, 1)',
-    'rgba(0, 63, 255, 1)',
-    'rgba(0, 0, 255, 1)',
-    'rgba(0, 0, 223, 1)',
-    'rgba(0, 0, 191, 1)',
-    'rgba(0, 0, 159, 1)',
-    'rgba(0, 0, 127, 1)',
-    'rgba(63, 0, 91, 1)',
-    'rgba(127, 0, 63, 1)',
-    'rgba(191, 0, 31, 1)',
-    'rgba(255, 0, 0, 1)'
-  ];
+
 
 
   
